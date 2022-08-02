96372c661d1b47d084b0e5b1ca8bf9be470cf623
joaolaureano <laureano.pedrojoao@gmail.com>
2022-08-02 19:02:16 -0300

    Added Input for user create and user exclusion

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,28 +1,40 @@
 const { usuarios, nextId } = require('../data/db')
 
+function indiceUsuario(filtro) {
+    if (!filtro) return -1;
+    const {id, email} = filtro
+    if (id) 
+        return usuarios.findIndex(u => u.id === id)
+    
+    if (email)
+        return usuarios.findIndex(u => u.email === email)
+
+    return -1
+}
+
+
 module.exports = {
-    novoUsuario(_, args) {
-        const existsEmail = usuarios.some(u => u.email === args.email)
+    novoUsuario(_, { dados }) {
+        const existsEmail = usuarios.some(u => u.email === dados.email)
         if (existsEmail) {
             throw new Error('Email já cadastrado')
         }
         const new_user = {
             id: nextId(),
-            ...args,
+            ...dados,
             perfil_id: 1,
             status: 'ATIVO'
         }
         usuarios.push(new_user)
         return new_user
     },
-    excluirUsuario(_, args) {
-        const index = usuarios.findIndex(u => u.id === args.id)
+    excluirUsuario(_, {filtro}) {
+        const index = indiceUsuario(filtro)
         if (index === -1) {
-            throw new Error('Usuário não encontrado')
+            return null
         }
-        const user = usuarios.splice(index, 1)[0]
-        
-        return user
+        const removed = usuarios.splice(index, 1)
+        return  removed ? removed[0] : null
     },
 
     alterarUsuario(_, args) {
