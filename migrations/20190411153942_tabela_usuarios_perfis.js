7528fe613ba5fa890db1d9399c9cc3bed846b4b9
joaolaureano <laureano.pedrojoao@gmail.com>
2022-08-02 21:16:55 -0300

    Created Insert query

--- migrations/20190411153942_tabela_usuarios_perfis.js
+++ migrations/20190411153942_tabela_usuarios_perfis.js
@@ -1,21 +1,14 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
+
+exports.up = function(knex, Promise) {
     return knex.schema.createTable('usuarios_perfis', table => {
         table.integer('usuario_id').unsigned()
         table.integer('perfil_id').unsigned()
         table.foreign('usuario_id').references('usuarios.id')
         table.foreign('perfil_id').references('perfis.id')
         table.primary(['usuario_id', 'perfil_id'])
-  })
+    })
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+exports.down = function(knex, Promise) {
     return knex.schema.dropTable('usuarios_perfis')
 };
