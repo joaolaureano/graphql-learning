872d561ff38671c0bd8eb01a9961cff91d4c75d3
joaolaureano <laureano.pedrojoao@gmail.com>
2022-08-02 21:49:52 -0300

    Added Usuario graphql query

--- resolvers/Query/usuario.js
+++ resolvers/Query/usuario.js
@@ -2,9 +2,15 @@ const db = require('../../config/db')
 
 module.exports = {
     async usuarios() {
-        // implementar
+        return await db('usuarios')
     },
     async usuario(_, { filtro }) {
-        // implementar
-    },
+        if (!filtro) return null
+        const { id, email } = filtro
+        if (id)
+            return db('perfis').where({ id }).first()
+        if(email)
+            return db('perfis').where({ email }).first()
+        return null
+    }
 }
