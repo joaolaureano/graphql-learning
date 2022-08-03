652f5a89250a0bd6c3f8c8f9c3ce9a76c5cba28e
joaolaureano <laureano.pedrojoao@gmail.com>
2022-08-02 22:23:21 -0300

    Mutation for profile

--- resolvers/Mutation/perfil.js
+++ resolvers/Mutation/perfil.js
@@ -1,11 +1,31 @@
 const db = require('../../config/db')
-
+const {perfil: obterPerfil} = require('../Query/perfil')
+const tableName = 'perfis'
 module.exports = {
     async novoPerfil(_, { dados }) {
-        // implementar
+        try {
+            const id = await db(tableName).insert(dados,'id')
+            return db(tableName).where({id: String(id)}).first()
+        }
+        catch (e) {
+            console.log(e)
+            throw new Error(e)
+        }
     },
     async excluirPerfil(_, { filtro }) {
-        // implementar
+        try {
+            const perfil = await obterPerfil(_, { filtro })
+            if (perfil) {
+                const perfil_id = perfil.id
+                await db('usuarios_perfis').where({ perfil_id }).delete()
+                await db(tableName).where({id: perfil_id}).delete()
+            }
+            return perfil
+
+        } catch (e) {
+            console.log(e)
+            throw new Error(e)
+        }
     },
     async alterarPerfil(_, { filtro, dados }) {
         // implementar
